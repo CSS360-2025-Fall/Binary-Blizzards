1805473d91d4ddbc75ee909deaf8acf52295ec79
kayla-kubicke <161649341+kayla-kubicke@users.noreply.github.com>
2025-11-12 22:50:33 +0000

    Example Object String Shows in Discord

--- card.js
+++ card.js
@@ -1,6 +1,9 @@
 export class Card {
-  // NOTE: These maps could be moved to a storage file later if
-  // they become annoying, but let's keep them in here for now.
+  // NOTE:
+  // If you need to learn more about these objects, they are called javascript Maps.
+  // These maps could be moved to a storage file later if they become annoying,
+  // but let's keep them in here for now.
+  // Example call: suits.spade.name; returns 'Spade'
     static suits = {
       spade: {
         name: 'Spade',
@@ -28,6 +31,11 @@ export class Card {
       },
     };
 
+    // NOTE:
+    // Call numerical values with []
+    // Example call: Card.ranks.game.blackjack[2].value; returns 2
+    // Call alpha character(s) with .
+    // Example call: Card.ranks.game.blackjack.J.value; returns 10
     static ranks = {
       game: {
         blackjack: {
@@ -35,7 +43,7 @@ export class Card {
             value: 1, // 11/10 FIX: Add method/logic to determine if value is 1 or 11
           },
           2: {
-            value: 2, // NOTE: Call with [], example: Card.ranks.game.blackjack[2].value;
+            value: 2,
           },
           3: {
             value: 3,
@@ -62,7 +70,7 @@ export class Card {
             value: 10,
           },
           J: {
-            value: 10, // NOTE: Call with ., example: Card.ranks.game.blackjack.J.value;
+            value: 10,
           },
           Q: {
             value: 10,
@@ -70,6 +78,9 @@ export class Card {
           K: {
             value: 10,
           },
+          // Joker: {
+          //   value: 11, // Include if playing Carnival Variant
+          // }
         },
       },
     };
@@ -81,19 +92,32 @@ export class Card {
   // instead of adding it to the game logic. Let's try it for now.
   constructor(suit, rank) {
     this.suit = suit;
-    this.rank = rank; // check when auto gen
+    this.rank = suit; // check when auto gen
   }
 
+  // NOTE: Decided to give this responsibility to Card class.
+  // One could make the arguement that this creates a deck and should
+  // live in the Deck class. That's fair and it could be moved.
+  // But, I'd argue that Deck class should be about the Deck's behavior
+  // such as shuffling and manipulating the order of card stack.
   static createCards() {
 
-    for(let key in Card.suits) {
-      this.suit = Card.suits.key;
-      for(let key in Card.ranks.game.blackjack) { // NOTE: Assumes game is blackjack for now.
-        this.rank = Card.ranks.key;
+    // A Map object can maintain order, but reordering a Map gets weird.
+    // Using an array is slower but allows us to manipulate the order easily.
+    let cardStack = [];
+
+    for(let suit in Card.suits) {
+      let currentSuit = Card.suits[suit];
+      for(let rank in Card.ranks.game.blackjack) { // FIX: Assumes game is blackjack for now.
+        let currentRank = Card.ranks[rank];
+        let currentCard = new Card(currentSuit,  currentRank);
+        cardStack.push(currentCard);
       }
     }
 
-    return this;
+    let example = cardStack[0];
+
+    return example.suit.name;
   }
 
   toString() {
